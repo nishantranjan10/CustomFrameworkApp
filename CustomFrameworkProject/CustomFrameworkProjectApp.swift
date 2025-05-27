090fa917429bcd7e9168caf6081d6ec9d9cd36cf
Nishant Ranjan <nishantranjan10@gmail.com>
2025-05-27 18:52:36 +0800

    Swift package added

--- CustomFrameworkProject/CustomFrameworkProjectApp.swift
+++ CustomFrameworkProject/CustomFrameworkProjectApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CustomFramework
 
 @main
 struct CustomFrameworkProjectApp: App {
--- CustomFrameworkProject/SDK.swift
+++ CustomFrameworkProject/SDK.swift
@@ -5,10 +5,13 @@
 //  Created by Nishant Ranjan on 27/5/25.
 //
 
-import Foundation
-
-struct SDK {
-    static func doSomeWork() {
-        print("Doing some work")
-    }
-}
+//import Foundation
+//
+//struct SDK {
+//    static func doSomeWork() {
+//        print("Doing some work")
+//    }
+//    static func doSomeWork1() {
+//        print("Doing some work")
+//    }
+//}
